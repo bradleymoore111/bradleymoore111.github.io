96474896a7166864c483e2fec01fc79aa915ee43
Bradley Gallon <bradleym007@gmail.com>
2014-04-29 12:44:43 -0700

    cleaned up links, made image

---
+++ js/link.js
@@ -0,0 +1,3 @@
+function loadLinks () {
+	document.getElementById("links").innerHTML = '<div id="linksContent"><a href="../index.html">Home</a><br><a href="../math/index.html">Math</a><br><a href="../msuic/index.html">Msuic</a><br><a href="../encryption/index.html">Encryption</a><br><a href="../love/index.html">Love</a><br></div>'
+}
