e465fe3383414c6c38cc660b56e708592614f19d
trashy111 <bradleym007@gmail.com>
2014-05-01 21:12:51 -0700

    Update link.js

--- js/link.js
+++ js/link.js
@@ -1,3 +1,3 @@
 function loadLinks () {
-	document.getElementById("links").innerHTML = '<div id="linksContent"><a href="../index.html">Home</a><br><a href="../math/index.html">Math</a><br><a href="../msuic/index.html">Msuic</a><br><a href="../encryption/index.html">Encryption</a><br><a href="../love/index.html">Love</a><br></div>'
+	document.getElementById("links").innerHTML = '<link rel="icon" type="image/gif" href="images/trashysweg.png"/><div id="linksContent"><a href="../index.html">Home</a><br><a href="../math/index.html">Math</a><br><a href="../msuic/index.html">Msuic</a><br><a href="../encryption/index.html">Encryption</a><br><a href="../love/index.html">Love</a><br></div>'
 }
