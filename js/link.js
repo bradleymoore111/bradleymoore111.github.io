e642f0073bd3b44ddb38b866d5c25a9b812f7a55
trashy111 <bradleym007@gmail.com>
2014-05-01 21:39:47 -0700

    fixed icon, removed links html, fixed crashing of log

--- js/link.js
+++ js/link.js
@@ -1,3 +1,9 @@
 function loadLinks () {
-	document.getElementById("links").innerHTML = '<link rel="icon" type="image/gif" href="images/trashysweg.png"/><div id="linksContent"><a href="../index.html">Home</a><br><a href="../math/index.html">Math</a><br><a href="../msuic/index.html">Msuic</a><br><a href="../encryption/index.html">Encryption</a><br><a href="../love/index.html">Love</a><br></div>'
+	document.getElementById("links").innerHTML = '<div id="linksContent"><a href="../index.html">Home</a><br><a href="../math/index.html">Math</a><br><a id="msuicLink" href="../msuic/index.html"></a><br><a href="../encryption/index.html">Encryption</a><br><a href="../love/index.html">Love</a><br></div>'
+
+	if(Math.random()<.5){
+		document.getElementById("msuicLink").innerText = "Msuic";
+	}else{
+		document.getElementById("msuicLink").innerText = "Music";
+	}
 }
