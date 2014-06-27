3af81839c4647d593205b7a522e9c34c2dd05eb4
trashy111 <bradleym007@gmail.com>
2014-06-27 08:02:39 -0700

    reverted back to working links

--- js/link.js
+++ js/link.js
@@ -1,6 +1,5 @@
 function loadLinks () {
 	document.getElementById("links").innerHTML = '<div id="linksContent"><a href="../index.html">Home</a><br><a href="../math/index.html">Math</a><br><a id="msuicLink" href="../msuic/index.html"></a><br><a href="../encryption/index.html">Encryption</a><br><a href="../love/index.html">Love</a><br></div>'
-
 	if(Math.random()<.5){
 		document.getElementById("msuicLink").innerText = "Msuic";
 	}else{
